2ead0f5510feae389cd65779f7ff9ea8a2b0b512
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-04 10:29:54 +0100

    style(section): margin has been reduced for main screens

--- src/ui/Section.tsx
+++ src/ui/Section.tsx
@@ -2,7 +2,7 @@ import Header from "./Header";
 
 export default function Section({ children, number, title }: { children: React.ReactNode, number: string, title: string }) {
     return (
-        <section className="m-8 md:m-24">
+        <section className="m-8 md:m-12">
             <div className="max-w-7xl mx-auto">
                 <Header number={number} title={title} />
                 {children}
