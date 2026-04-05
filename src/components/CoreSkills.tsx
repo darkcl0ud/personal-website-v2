6bbd05f1ab2bfb29684e52976f4bf305c871fc68
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-05 22:30:31 +0100

    style(skills): added core skills and changed box scaling to have a min width instead of fixed.

--- src/components/CoreSkills.tsx
+++ src/components/CoreSkills.tsx
@@ -1,4 +1,4 @@
-import { ShieldCheckIcon, CloudIcon, CubeTransparentIcon, GlobeAmericasIcon } from "@heroicons/react/24/outline";
+import { ShieldCheckIcon, CloudIcon, CubeTransparentIcon, GlobeAmericasIcon, DocumentChartBarIcon, WrenchScrewdriverIcon} from "@heroicons/react/24/outline";
 import Section from "../ui/Section";
 import Header from "../ui/Header";
 
@@ -23,17 +23,27 @@ const coreSkills = [
         description: "Design solutions that are optimized for global reach and scalability. Ensuring that solutions are designed to handle high traffic and scale with the business. ",
         icon: <GlobeAmericasIcon className="w-8 h-8 text-primary-100" />
     },
+    {
+        title: "Architectural Patterns",
+        description: "Design solutions following real world architectural patterns and best practices.",
+        icon: <DocumentChartBarIcon className="w-8 h-8 text-primary-100" />
+    },
+    {
+        title: "Diagnosis",
+        description: "Diagnose and troubleshoot complex issues with a deep understanding of the underlying infrastructure and systems.",
+        icon: <WrenchScrewdriverIcon className="w-8 h-8 text-primary-100" />
+    },
 ]
 
 export default function CoreSkills() {
     return <Section>
         <Header number="01" title="Core Skills" />
-        <div className="mx-auto grid w-full grid-cols-[repeat(auto-fit,minmax(min(100%,24rem),24rem))] justify-center">
+        <div className="mx-auto grid w-full grid-cols-[repeat(auto-fit,minmax(min(100%,24rem),1fr))] justify-center">
             {coreSkills.map((skill) => (
                 <div key={skill.title} className="bg-neutral-500 p-8 outline outline-white/5 min-h-80 flex flex-col gap-8">
                     {skill.icon}
                     <h2 className="text-2xl font-headline text-white tracking-wide">{skill.title}</h2>
-                    <p className="text-tertiary-100 font-light tracking-widest">{skill.description}</p>
+                    <p className="text-tertiary-100 font-extralight">{skill.description}</p>
                 </div>
             ))}
         </div>
