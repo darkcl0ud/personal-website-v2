a2dfc40d41317a1231f1174f206dea7fe87f7c8e
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-05 23:06:30 +0100

    style(footer): menu reverse switch on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
     return (
         <footer className="w-full min-h-16 border-t-1 border-primary-100/20 mt-auto">
-            <div className="max-w-[1440px] mx-auto h-full p-4 flex md:flex-row flex-col items-center justify-between font-label text-xs sm:gap-4 gap-6">
+            <div className="max-w-[1440px] mx-auto h-full p-4 flex md:flex-row flex-col-reverse items-center justify-between font-label text-xs sm:gap-4 gap-6">
+                <p className="text-primary-100 font-light tracking-widest text-center md:text-left">© 2026 Liam Hughes // BUILT_ON_THE_GRID</p>
                 <ul className="flex flex-col sm:flex-row sm:gap-4 gap-6 text-tertiary-100 font-light tracking-widest text-center">
                     <li>
                         <a href="mailto:liam@nyx.technology" target="_blank" rel="noopener noreferrer">Email</a>
@@ -16,7 +17,6 @@ export default function Footer() {
                         <a href="https://www.linkedin.com/in/darkcl0ud/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                     </li>
                 </ul>
-                <p className="text-primary-100 font-light tracking-widest text-center md:text-left">© 2026 Liam Hughes // BUILT_ON_THE_GRID</p>
             </div>
         </footer>
     )
