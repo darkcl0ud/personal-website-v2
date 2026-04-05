6378eb83a28dbe19b861d55a2c3a19bfef4ba39b
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-05 22:31:12 +0100

    style(systemstatus): removed border

--- src/ui/SystemStatus.tsx
+++ src/ui/SystemStatus.tsx
@@ -1,6 +1,6 @@
 export default function SystemStatus() {
     return (
-        <div className="max-w-4xl text-center font-label bg-[color-mix(in_oklab,var(--color-primary-100)_40%,black)] px-4 py-2 outline outline-white/20">
+        <div className="max-w-4xl text-center font-label bg-[color-mix(in_oklab,var(--color-primary-100)_40%,black)] px-4 py-2">
             <p className="text-xs md:text-sm xl:text-base text-white/40 font-light tracking-widest">SYSTEM_STATUS: <span className="text-primary-100 font-light">OPERATIONAL</span></p>
         </div>
     )
