53bbfa54fe67b13a44c1d6c2b293fdee3352b414
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-06 09:02:06 +0100

    fix(section): scaling non has same max for content as the nav and home hero

--- src/ui/Section.tsx
+++ src/ui/Section.tsx
@@ -1,6 +1,6 @@
 export default function Section({ children }: { children: React.ReactNode }) {
     return (
-        <section className="m-8 md:m-12">
+        <section className="my-8 md:my-12 px-8 md:px-24">
             <div className="max-w-[1440px] mx-auto">
                 {children}
             </div>
