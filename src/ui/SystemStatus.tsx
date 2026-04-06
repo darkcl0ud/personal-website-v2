be5b13d9d81e5ccd796e2bfbaeed82224e135869
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-06 19:05:37 +0100

    style(hero): home hero status component changed to resemble new design

--- src/ui/SystemStatus.tsx
+++ src/ui/SystemStatus.tsx
@@ -1,7 +1,5 @@
 export default function SystemStatus() {
     return (
-        <div className="max-w-4xl text-center font-label bg-[color-mix(in_oklab,var(--color-primary-100)_40%,black)] px-4 py-2">
-            <p className="text-xs md:text-sm xl:text-base text-white/40 font-light tracking-widest">SYSTEM_STATUS: <span className="text-primary-100 font-light">OPERATIONAL</span></p>
-        </div>
+        <p className="text-xs md:text-sm text-white/40 font-label font-light tracking-widest">SYSTEM_STATUS: <span className="text-primary-100 font-light">OPERATIONAL</span></p>
     )
 }
