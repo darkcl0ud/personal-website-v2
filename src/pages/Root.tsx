8077d693fd582b7a34ccb4057d05c75bf5e6ebc0
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-06 20:06:27 +0100

    build(scroll): scroll up on new page

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import { ScrollUpOnNewPage } from "../ui/ScrollUpOnNewPage";
 
 export default function RootPage() {
     return (
@@ -8,6 +9,7 @@ export default function RootPage() {
             <Nav />
             <Outlet />
             <Footer />
+            <ScrollUpOnNewPage />
         </div>
     )
 }
