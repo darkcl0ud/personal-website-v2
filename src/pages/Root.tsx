303c886ce13b05cebc93d040a6c2b02ca950326b
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-06 10:28:39 +0100

    fix(root): used grid to fill outlet in middle

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 
 export default function RootPage() {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="grid grid-rows-[auto_1fr_auto] min-h-screen">
             <Nav />
             <Outlet />
             <Footer />
