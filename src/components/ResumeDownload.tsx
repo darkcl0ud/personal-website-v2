ab796070d91bb923168a1959df460a911bf33260
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-07 21:40:19 +0100

    build(resume): added ability to download resume

--- src/components/ResumeDownload.tsx
+++ src/components/ResumeDownload.tsx
@@ -7,7 +7,7 @@ export default function ResumeDownload() {
         <Section>
             <div className="flex flex-col items-center justify-center gap-8">
                 <p className="text-white font-extralight tracking-widest text-xs animate-pulse">END_OF_LOGS // READY_FOR_TRANSFER</p>
-                <Button variant="secondary"><span className="flex items-center gap-2"><ArrowDownTrayIcon className="w-4 h-4" /> DOWNLOAD_RESUME</span></Button>
+                <a href="/media/resume" download><Button variant="secondary"><span className="flex items-center gap-2"><ArrowDownTrayIcon className="w-4 h-4" /> DOWNLOAD_RESUME</span></Button></a>
             </div>
         </Section>
     )
