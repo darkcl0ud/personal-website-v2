422cf3f2306d02439044a30beea5db46e68613ec
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-08 10:47:25 +0100

    refactor(locate): removed usestate completley

--- src/components/IPBox.tsx
+++ src/components/IPBox.tsx
@@ -5,8 +5,6 @@ import type { RootState } from "../store/index.ts";
 import { locateActions } from "../store/locate.ts";
 
 export default function IPBox() {
-    const [ip, setIp] = useState<string | null>(null);
-    const [country, setCountry] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
     const userLocate = useSelector((state: RootState) => state.locate);
     const dispatch = useDispatch();
@@ -15,28 +13,22 @@ export default function IPBox() {
     const fetchIP = async () => {
       try {
         const data = await locateAction();
-        setIp(data.ip);
-        setCountry(data.country);
         dispatch(locateActions.setLocate({ ip: data.ip, country: data.country }));
       } catch (err) {
         console.error(err);
-        setIp("ERROR");
-        setCountry("ERROR");
+        dispatch(locateActions.setLocate({ ip: "ERROR", country: "ERROR" }));
       } finally {
         setLoading(false);
       }
     };
-    if (userLocate.ip) {
-        setIp(userLocate.ip);
-        setCountry(userLocate.country);
-    } else {
+    if (!userLocate.ip) {
       fetchIP();
     }
   }, []);
     return (
         <div className="w-fit h-20 md:absolute md:bottom-0 md:right-0 font-label font-light">
-            <p className="text-xs md:text-sm text-white/40 tracking-widest">IP_ADDR: <span>{loading ? "Loading..." : ip}</span></p>
-            <p className="text-xs md:text-sm text-white/40 tracking-widest">LOC: <span>{loading ? "Loading..." : country}</span></p>
+            <p className="text-xs md:text-sm text-white/40 tracking-widest">IP_ADDR: <span>{loading ? "Loading..." : userLocate.ip}</span></p>
+            <p className="text-xs md:text-sm text-white/40 tracking-widest">LOC: <span>{loading ? "Loading..." : userLocate.country}</span></p>
             <p className="text-xs md:text-sm text-white/40 tracking-widest">VER: v1.0.0</p>
         </div>
     )
