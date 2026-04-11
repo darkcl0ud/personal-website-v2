d11172c4e9615c0723dd336067f9df6b7a22273e
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-11 10:18:20 +0100

    build(article): changed id format for articles

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -58,10 +58,19 @@ const components: Components = {
   },
 }
 
+function getDate(id: string) {
+  const year   = parseInt(id.substring(0, 4), 10);
+  const month  = parseInt(id.substring(4, 6), 10) - 1; // JS months are 0-indexed
+  const day    = parseInt(id.substring(6, 8), 10);
+  const hour   = parseInt(id.substring(8, 10), 10);
+  const minute = parseInt(id.substring(10, 12), 10);
+  return new Date(year, month, day, hour, minute);
+}
+
 export default function Article() {
   const { id } = useParams();
   const { content, headers } = useLoaderData<typeof articleLoader>();
-  const date = new Date(parseInt(headers.id + "000"));
+  const date = getDate(headers.id);
   const formattedDate = date.toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" });
 
 
