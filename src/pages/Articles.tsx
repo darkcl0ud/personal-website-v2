a8b5c4f5c71a54aa6d90e0f21c1c98031fb0986f
Liam <65425528+darkcl0ud@users.noreply.github.com>
2026-04-27 12:55:12 +0100

    chore(seo): reduce articles page description length

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -7,7 +7,7 @@ export default function Articles() {
 
     return (
         <div>
-            <SetMeta title="Liam David Hughes - Articles" description="Technical articles with different solutions built in the cloud and on local machines. Topics include cloud architecture, serverless, and more. Platforms such as AWS and Cloudflare are documented." keywords="Liam David Hughes, articles, blogs, technical logs" image="https://liam.network/favicon.svg" type="article" />
+            <SetMeta title="Liam David Hughes - Articles" description="Technical articles with different solutions built locally and in the cloud. Topics include Cloudflare, AWS, serverless, Docker, cybersecurity and others." keywords="Liam David Hughes, articles, blogs, technical logs" image="https://liam.network/favicon.svg" type="article" />
             <TechnicalLogsTitle />
             <TechnicalLogs />
         </div>
